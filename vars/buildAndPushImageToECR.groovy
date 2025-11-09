cf47373788b99e192f1b7b037d1f8cb30cb58acd
mashoodkhan <70470042+mashoodkhan@users.noreply.github.com>
2025-11-09 15:19:27 +0530

    Update buildAndPushImageToECR.groovy

--- vars/buildAndPushImageToECR.groovy
+++ vars/buildAndPushImageToECR.groovy
@@ -1,26 +1,19 @@
-def call(project, tag, awsAccountId, awsRegion, contextDir) {
+def buildAndPushImageToECR(project, tag, awsAccountId, awsRegion, contextDir) {
 
     def ecrRepo = "${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${project}"
 
-    // Build
+    // Build Image
     sh "docker build -t ${project}:${tag} ${contextDir}"
 
+    // Login to ECR
     withAWS(credentials: 'aws-creds', region: awsRegion) {
-
-        // create repo if not exists
-        sh """
-            aws ecr describe-repositories --repository-names ${project} \
-            || aws ecr create-repository --repository-name ${project} --region ${awsRegion}
-        """
-
-        // Login Docker to ECR
         sh """
             aws ecr get-login-password --region ${awsRegion} \
             | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com
         """
     }
 
-    // Tag & Push (this does not require AWS creds)
+    // Tag & Push
     sh "docker tag ${project}:${tag} ${ecrRepo}:${tag}"
     sh "docker push ${ecrRepo}:${tag}"
 
